8e26a55046afec2fb3bb74c9d31f1cf1d664bc0d
Denis Gamidov <dgamidov@gmail.com>
2017-01-27 16:01:02 +0300

    Add application Security with mix new security

---
+++ apps/security/lib/security.ex
@@ -0,0 +1,18 @@
+defmodule Security do
+  @moduledoc """
+  Documentation for Security.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Security.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/security/test/security_test.exs
@@ -0,0 +1,8 @@
+defmodule SecurityTest do
+  use ExUnit.Case
+  doctest Security
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
